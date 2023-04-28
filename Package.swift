2af4c29f1408b0b3df2f8fa79c51ecba979cc3f9
Wade Tregaskis <wadetregaskis@mac.com>
2023-04-27 18:00:37 -0700

    Added nominal support for tvOS and watchOS, although I have only tested the code in simulators, not on real devices.  It wouldn't surprise me if tvOS and/or watchOS don't expose getifaddrs.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,9 @@ let package = Package(
     platforms: [
         .iOS(.v15),
         .macOS(.v12),
-        .macCatalyst(.v15)
+        .macCatalyst(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         .library(
