c819aed5bd2c9463b78d24f015668782700e2df7
Wade Tregaskis <wadetregaskis@mac.com>
2023-04-29 13:06:29 -0700

    Added a little more detail on the specific fields changed, in the demonstration test case.

--- Tests/NetworkInterfaceChangeMonitoringTests/NetworkInterfaceChangeMonitoringTests.swift
+++ Tests/NetworkInterfaceChangeMonitoringTests/NetworkInterfaceChangeMonitoringTests.swift
@@ -7,13 +7,22 @@ final class NetworkInterfaceChangeMonitoringTests: XCTestCase {
     func testInProduction🤪() async throws {
         // Note that this never terminates - you'll have to kill the test manually when you're done.
         for try await change in NetworkInterface.changes {
+            let changedFields: NetworkInterface.Change.ModificationNature
+
+            switch change.nature {
+            case .modified(let modificationNature):
+                changedFields = modificationNature
+            default:
+                changedFields = []
+            }
+
             print("""
                   \(change.interface.name) \(change.nature):
-                      Address: \(change.interface.address.orNilString) (\((change.interface.address?.family).orNilString))
-                      Netmask: \(change.interface.netmask.orNilString)
-                      Broadcast: \(change.interface.broadcastAddress.orNilString)
-                      Destination: \(change.interface.destinationAddress.orNilString)
-                      Flags: \(change.interface.flags)
+                   \(changedFields.contains(.address) ? "🔔" : "  ") Address: \(change.interface.address.orNilString) (\((change.interface.address?.family).orNilString))
+                   \(changedFields.contains(.netmask) ? "🔔" : "  ") Netmask: \(change.interface.netmask.orNilString)
+                   \(changedFields.contains(.broadcastAddress) ? "🔔" : "  ") Broadcast: \(change.interface.broadcastAddress.orNilString)
+                   \(changedFields.contains(.destinationAddress) ? "🔔" : "  ") Destination: \(change.interface.destinationAddress.orNilString)
+                   \(changedFields.contains(.flags) ? "🔔" : "  ") Flags: \(change.interface.flags)
                   """)
         }
     }
