4ffdce9cb5c85327d2fcf7465cee7e3740203f12
Wade Tregaskis <wadetregaskis@mac.com>
2023-04-29 16:12:49 -0700

    Added a 60 second timeout to the NetworkInterfaceChangeMonitoring `test`, so that it won't hang the build & test workflow on GitHub & Swift Package Index.

--- Tests/NetworkInterfaceChangeMonitoringTests/NetworkInterfaceChangeMonitoringTests.swift
+++ Tests/NetworkInterfaceChangeMonitoringTests/NetworkInterfaceChangeMonitoringTests.swift
@@ -5,25 +5,30 @@ import NetworkInterfaceInfo
 
 final class NetworkInterfaceChangeMonitoringTests: XCTestCase {
     func testInProduction🤪() async throws {
-        // Note that this never terminates - you'll have to kill the test manually when you're done.
-        for try await change in NetworkInterface.changes {
-            let changedFields: NetworkInterface.Change.ModificationNature
+        let task = Task {
+            for try await change in NetworkInterface.changes {
+                let changedFields: NetworkInterface.Change.ModificationNature
 
-            switch change.nature {
-            case .modified(let modificationNature):
-                changedFields = modificationNature
-            default:
-                changedFields = []
+                switch change.nature {
+                case .modified(let modificationNature):
+                    changedFields = modificationNature
+                default:
+                    changedFields = []
+                }
+
+                print("""
+                      \(change.interface.name) \(change.nature):
+                       \(changedFields.contains(.address) ? "🔔" : "  ") Address: \(change.interface.address.orNilString) (\((change.interface.address?.family).orNilString))
+                       \(changedFields.contains(.netmask) ? "🔔" : "  ") Netmask: \(change.interface.netmask.orNilString)
+                       \(changedFields.contains(.broadcastAddress) ? "🔔" : "  ") Broadcast: \(change.interface.broadcastAddress.orNilString)
+                       \(changedFields.contains(.destinationAddress) ? "🔔" : "  ") Destination: \(change.interface.destinationAddress.orNilString)
+                       \(changedFields.contains(.flags) ? "🔔" : "  ") Flags: \(change.interface.flags)
+                      """)
             }
-
-            print("""
-                  \(change.interface.name) \(change.nature):
-                   \(changedFields.contains(.address) ? "🔔" : "  ") Address: \(change.interface.address.orNilString) (\((change.interface.address?.family).orNilString))
-                   \(changedFields.contains(.netmask) ? "🔔" : "  ") Netmask: \(change.interface.netmask.orNilString)
-                   \(changedFields.contains(.broadcastAddress) ? "🔔" : "  ") Broadcast: \(change.interface.broadcastAddress.orNilString)
-                   \(changedFields.contains(.destinationAddress) ? "🔔" : "  ") Destination: \(change.interface.destinationAddress.orNilString)
-                   \(changedFields.contains(.flags) ? "🔔" : "  ") Flags: \(change.interface.flags)
-                  """)
         }
+
+        // This is really just so that automated tests (e.g. on GitHub) don't run forever.  Best to add a bunch more zeroes to it when actually using it locally.
+        try await Task.sleep(nanoseconds: 60_000_000_000)
+        task.cancel()
     }
 }
